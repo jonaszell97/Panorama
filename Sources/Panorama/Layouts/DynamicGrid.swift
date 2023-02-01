f867313cd95a59f3d99783d74293318834514795
Jonas Zell <zell.jonas97@gmail.com>
2023-02-01 10:59:25 +0100

    * add DynamicGrid layout * add progress bar view * add onAppearOrChange modifier

---
+++ Sources/Panorama/Layouts/DynamicGrid.swift
@@ -0,0 +1,184 @@
+
+import SwiftUI
+
+public struct _DynamicGridLayoutCache {
+    struct Row {
+        /// The indices of the subviews in this row.
+        let subviewIndices: [(Int, CGFloat)]
+        
+        /// The size of this row's content.
+        let size: CGSize
+    }
+    
+    /// List of rows in this layout.
+    var rows: [Row] = []
+    
+    /// The total height of the layout.
+    var totalHeight: CGFloat = 0
+}
+
+@available(iOS 16, macOS 13, *)
+public struct DynamicGrid: Layout {
+    /// The alignment of the grid.
+    let alignment: HorizontalAlignment
+    
+    /// The spacing between elements.
+    let spacing: CGFloat
+    
+    /// Default initializer.
+    public init(alignment: HorizontalAlignment = .center, spacing: CGFloat = 10) {
+        self.alignment = alignment
+        self.spacing = spacing
+    }
+    
+    public static var layoutProperties: LayoutProperties {
+        var properties = LayoutProperties()
+        properties.stackOrientation = .vertical
+        return properties
+    }
+    
+    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout _DynamicGridLayoutCache) -> CGSize {
+        // Pass the size proposal on to the subviews
+        let subviewSizes = subviews.map { $0.sizeThatFits(proposal) }
+        
+        // If the width is unbounded, return a single row containing all subviews
+        guard let proposalWidth = proposal.width, proposalWidth.isFinite else {
+            let maxHeight = subviewSizes.max { $0.height < $1.height }?.height ?? 0
+            let totalWidth = subviewSizes.reduce(0) { $0 + $1.width }
+            
+            return .init(width: totalWidth, height: maxHeight)
+        }
+        
+        cache = .init()
+        
+        var currentRowWidth: CGFloat = 0
+        var currentRowHeight: CGFloat = 0
+        var currentRow = [(Int, CGFloat)]()
+        
+        for (i, subviewSize) in subviewSizes.enumerated() {
+            let subviewWidth = subviewSize.width
+            let subviewWidthWithSpacing: CGFloat
+            
+            // Add spacing for every view but the first
+            if currentRowWidth > 0 {
+                subviewWidthWithSpacing = subviewWidth + self.spacing
+            }
+            else {
+                subviewWidthWithSpacing = subviewWidth
+            }
+            
+            let subviewHeight = subviewSize.height
+            
+            // View can fit on this row
+            if currentRowWidth + subviewWidthWithSpacing <= proposalWidth {
+                currentRow.append((i, subviewWidth + self.spacing))
+                currentRowWidth += subviewWidthWithSpacing
+                currentRowHeight = max(currentRowHeight, subviewHeight)
+            }
+            // Start new row
+            else {
+                cache.rows.append(.init(subviewIndices: currentRow, size: .init(width: currentRowWidth, height: currentRowHeight)))
+                cache.totalHeight += currentRowHeight
+                
+                currentRow = [(i, subviewWidth + self.spacing)]
+                currentRowWidth = subviewWidth
+                currentRowHeight = subviewHeight
+            }
+        }
+        
+        if !currentRow.isEmpty {
+            cache.rows.append(.init(subviewIndices: currentRow, size: .init(width: currentRowWidth, height: currentRowHeight)))
+            cache.totalHeight += currentRowHeight
+        }
+        
+        // Add vertical spacing
+        if !cache.rows.isEmpty {
+            cache.totalHeight += CGFloat(cache.rows.count - 1) * self.spacing
+        }
+        
+        return .init(width: proposalWidth, height: cache.totalHeight)
+    }
+    
+    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout _DynamicGridLayoutCache) {
+        var currentVerticalOffset: CGFloat = bounds.origin.y // - (bounds.height - (proposal.height ?? bounds.height))
+        for row in cache.rows {
+            var currentHorizontalOffset: CGFloat = bounds.minX
+            switch self.alignment {
+            case .leading:
+                currentHorizontalOffset += 0
+            case .trailing:
+                currentHorizontalOffset += bounds.width - row.size.width
+            case .center:
+                fallthrough
+            default:
+                currentHorizontalOffset += (bounds.width - row.size.width) * 0.5
+            }
+            
+            for (subviewIndex, subviewWidth) in row.subviewIndices {
+                subviews[subviewIndex].place(at: .init(x: currentHorizontalOffset, y: currentVerticalOffset),
+                                             proposal: proposal)
+                
+                currentHorizontalOffset += subviewWidth
+            }
+            
+            currentVerticalOffset += row.size.height
+            currentVerticalOffset += self.spacing
+        }
+    }
+    
+    public func makeCache(subviews: Subviews) -> _DynamicGridLayoutCache {
+        .init()
+    }
+    
+    public func updateCache(_ cache: inout _DynamicGridLayoutCache, subviews: Subviews) {
+        cache = .init()
+    }
+}
+
+@available(iOS 16, macOS 13, *)
+struct DynamicGridPreviews: PreviewProvider {
+    struct TagView: View {
+        let text: String
+        var body: some View {
+            Text(verbatim: text)
+                .foregroundColor(.white)
+                .lineLimit(1)
+//                .fixedSize(horizontal: true, vertical: true)
+                .padding(.vertical, 5)
+                .padding(.horizontal)
+                .background(
+                    RoundedRectangle(cornerRadius: (UIFont.preferredFont(forTextStyle: .body).pointSize + 10) * 0.5)
+                        .fill(Color.cyan)
+                )
+        }
+    }
+    
+    struct PreviewView: View {
+        var body: some View {
+            VStack {
+                Spacer()
+                DynamicGrid(alignment: .leading) {
+                    TagView(text: "X")
+                    TagView(text: "Y")
+                    TagView(text: "Z")
+                    TagView(text: "UIuiasda")
+                    TagView(text: "UIuiasdaasihdaosdU")
+                    TagView(text: "Z")
+                    TagView(text: "U ioasd ioasdio ajsoiasd")
+                    TagView(text: "Uidsi")
+                    TagView(text: "A")
+                    TagView(text: "B")
+                }
+                .frame(maxWidth: 150)
+                .background(Color.red)
+                
+                Spacer()
+            }
+            .frame(maxHeight: .infinity)
+        }
+    }
+    
+    static var previews: some View {
+        PreviewView()
+    }
+}
